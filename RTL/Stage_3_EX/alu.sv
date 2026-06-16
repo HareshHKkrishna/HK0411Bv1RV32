a5297ac8407a26c3e5043a1b15aa7fa35ee36b7e
Haresh Krishna G S <hareshkrishnagsachithanandam@gmail.com>
2026-06-16 11:57:09 +0530

    Complete update of June 15th 2026

---
+++ RTL/Stage_3_EX/alu.sv
@@ -0,0 +1,23 @@
+module alu (
+    input logic [31:0]a,
+    input logic [31:0]b,
+    input logic [3:0]alu_control,
+    output logic [31:0]result
+);
+    always_comb begin
+        case (alu_control)
+            4'b0000 : result = a+b;                   // ADD
+            4'b0001 : result = a-b;                   // SUB
+            4'b0010 : result = a<<b[4:0];                  // SLL
+            4'b0011 : result = $signed(a)<$signed(b); // SLT
+            4'b0100 : result = a<b;                   // SLTU
+            4'b0101 : result = a^b;                   // XOR
+            4'b0110 : result = a>>b[4:0];             // SRL
+            4'b0111 : result = $signed(a)>>>(b[4:0]);// SRA
+            4'b1000 : result = a|b;                   // OR
+            4'b1001 : result = a&b;                   // AND
+            4'b1010 : result = a;                     //PASS_IMM
+            default: result = 32'd0;                  // reserved
+        endcase
+    end
+endmodule
