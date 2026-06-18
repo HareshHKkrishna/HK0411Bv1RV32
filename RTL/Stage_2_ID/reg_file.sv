b2506c18d9f0a9cb2db9dc5849263bc9c5ea7f2a
Haresh Krishna G S <hareshkrishnagsachithanandam@gmail.com>
2026-06-18 22:08:23 +0530

    Complete update of June 18th 2026

--- RTL/Stage_2_ID/reg_file.sv
+++ RTL/Stage_2_ID/reg_file.sv
@@ -6,9 +6,33 @@ module reg_file(
     input logic wr_en,          // reg_write_enable.
     input logic clk,
     input logic rst,
-
-     
-
+    output logic [31:0]rs1_data,// Register 1 data.
+    output logic [31:0]rs2_data // Register 2 data.
 );
+    logic [31:0]registers[31:0];
+    always_ff @( posedge clk ) begin
+        
+        if(rst)begin
+            for(int i=0;i<32;i++)begin
+                registers[i]<=32'b0;
+            end
+        end
+
+        else if(wr_en && wr_addr!=0)begin
+            registers[wr_addr]<=wr_data;
+        end
+        
+    end
+
+    always_comb begin
+        if(rs1== 0)
+            rs1_data=0;
+        else
+            rs1_data=registers[rs1];
+        if(rs2==0)
+            rs2_data=0;
+        else
+            rs2_data=registers[rs2];
+    end
 
 endmodule
