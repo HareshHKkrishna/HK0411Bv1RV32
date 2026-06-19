5fe05bd7fe44967cdc9a9e9361593de4c1f5ee65
Haresh Krishna G S <hareshkrishnagsachithanandam@gmail.com>
2026-06-19 12:18:15 +0530

    Register File of the Processor. June 18th 2026

--- RTL/Stage_2_ID/reg_file.sv
+++ RTL/Stage_2_ID/reg_file.sv
@@ -8,20 +8,17 @@ module reg_file(
     input logic rst,
     output logic [31:0]rs1_data,// Register 1 data.
     output logic [31:0]rs2_data // Register 2 data.
-);
+);  
     logic [31:0]registers[31:0];
     always_ff @( posedge clk ) begin
-        
         if(rst)begin
             for(int i=0;i<32;i++)begin
                 registers[i]<=32'b0;
             end
         end
-
         else if(wr_en && wr_addr!=0)begin
             registers[wr_addr]<=wr_data;
-        end
-        
+        end    
     end
 
     always_comb begin
@@ -34,5 +31,4 @@ module reg_file(
         else
             rs2_data=registers[rs2];
     end
-
 endmodule
