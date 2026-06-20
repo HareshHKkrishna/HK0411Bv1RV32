c5a3b9556ffe6e136bee7f20020105b7cb338312
Haresh Krishna G S <hareshkrishnagsachithanandam@gmail.com>
2026-06-21 02:15:06 +0530

    BTG  v1

---
+++ RTL/Stage_3_EX/branch_target_gen.sv
@@ -0,0 +1,16 @@
+module branch_target_gen (
+    input  logic [31:0] imm,
+    input  logic [31:0] pc,
+    input  logic [31:0] pc4,
+    input  logic        branch_taken,
+    output logic [31:0] pc_next
+);
+
+    always_comb begin
+        if (branch_taken)
+            pc_next = pc + imm;
+        else
+            pc_next = pc4;
+    end
+
+endmodule
