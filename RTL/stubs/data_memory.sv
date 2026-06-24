762fe78c567c1544bded146bcd5f5e7c46ab0608
Haresh Krishna G S <hareshkrishnagsachithanandam@gmail.com>
2026-06-24 10:24:22 +0530

    stub memories used for Synthesis

---
+++ RTL/stubs/data_memory.sv
@@ -0,0 +1,11 @@
+module data_memory (
+    input  logic        clk,
+    input  logic [31:0] addr,
+    input  logic [31:0] wdata,
+    input  logic        we,
+    output logic [31:0] rdata
+);
+
+
+
+endmodule
---
+++ RTL/stubs/instruction_memory.sv
@@ -0,0 +1,11 @@
+module instruction_memory (
+    input logic [31:0]pc,
+    output logic [31:0]instr
+);
+
+endmodule
+
+
+
+
+
